dc03412b4ea962dbd74e827b90e29ebe0fba200a
nichro02 <nichro02@gmail.com>
2020-12-31 13:07:57 -0800

    Created Following component

---
+++ src/components/Following.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Following = () => {
+
+    return (
+        <>
+        <div>
+            <h1>Following Feed</h1>
+        </div>
+        </>
+    )
+}
+
+export default Following
