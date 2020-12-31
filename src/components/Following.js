733d4c4b863fdbd97721b442fa11f9bf544bc49b
nichro02 <nichro02@gmail.com>
2020-12-31 13:34:16 -0800

    Grabbed userId from local storage to use in get call for following feed

--- src/components/Following.js
+++ src/components/Following.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
+//import getCurrentUser to grab user ID
+import { getCurrentUser } from '../services/auth.service.js'
+//import service displaying posts from users a user follows
+import { viewFollowedPosts } from '../services/post.service.js'
+
 const Following = () => {
+
+    let user = getCurrentUser()
+    console.log(user.id)
+
 
     return (
         <>
