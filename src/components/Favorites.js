5d80eef4d5db158216754c6f50c9a0001c45922d
regularvanessaperson <69284928+regularvanessaperson@users.noreply.github.com>
2021-01-01 16:53:04 -0600

    Update favorites component (#12)
    * updated Favorites component can see in console log not rendering yet
    
    * updated Favorites.js still cannot get list of favorites to render
    
    * removed eslintcache from repo and added to git ignore
    
    * able to render posts on Favorites.js

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,21 +1,55 @@
-const Favorites = () => {
-    // const favePosts = () => {
-    //     // function that returns list of favePosts
-    //     return (
-    //         <ul>
-    //             <li>{posts.creator.name}</li>
-    //             <li>{posts.body}</li>
-    //         </ul>
+import React, { useEffect, useState } from 'react'
 
-    //     )
+//Helper
+import { viewFavoritePosts } from '../services/post.service'
+import { getCurrentUser } from '../services/auth.service'
+
+const Favorites = (props) => {
+    console.log("what are props", props)
+    const [favorites, setFavorites]= useState([])
+    const currentUser = getCurrentUser()
+    const id = currentUser.id
+    console.log(currentUser.id)
+    let postArray = []
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+       setLoading(false)
+       favePosts()
+    }, [])
+
+
+    async function favePosts() {
+      return await viewFavoritePosts(id).then(user => {
+            setLoading(false)
+            console.log(user.data.favoritePosts)
+            return user.data.favoritePosts.map(post => (
+                <ul>
+                    <li key={post._id}>
+                        {post.body}
+                    </li>
+                </ul>
+            ))
+        }).then(results=>{
+            setFavorites(results)
+        })
+    }
+
+
+    // if (loading) {
+    //     return <div>
+    //         Loading
+    //     </div>
     // }
 
+
     return <div>
-        Favorites
-        {/* this component will render a list of favorites by current user */}
-        
-         </div>
-    
+        <h1>Favorites</h1>
+        {favorites}
+
+    </div>
+
 }
 
 export default Favorites
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+
 const API_URL="http://localhost:8080/api/posts/"
 
 //create a new post
