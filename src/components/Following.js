3f84d9724d94e3af6f48cd3c6f0ed53e97ee8b2a
nichro02 <nichro02@gmail.com>
2020-12-31 19:55:06 -0800

    Following feed currently shows IDs for posts from users a user is following, but will either need to make another API call on frontend or update backend to populate the body of the posts

--- src/components/Following.js
+++ src/components/Following.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 //import getCurrentUser to grab user ID
 import { getCurrentUser } from '../services/auth.service.js'
 //import service displaying posts from users a user follows
 import { viewFollowedPosts } from '../services/post.service.js'
+//import spinner
+import ButtonSpinner from './common/ButtonSpinner'
 
 const Following = () => {
-    let followedPostData
+    //initialize state for fetching data
+    const [data, setData] = useState([])
+    // initialize state for loader
+    const [loading, setLoading] = useState(true)
+
+    
+    //let followedPostData
 
     const user = getCurrentUser()
     console.log(user.id)
-
+    /*
     const followingPosts = viewFollowedPosts(user.id)
 
     async function getPostData() {
@@ -19,17 +27,45 @@ const Following = () => {
     }
 
     getPostData()
-
-    /*
-    let followingPosts = viewFollowedPosts(user.id)
-    console.log(followingPosts)
-    console.log(followingPosts)
+    console.log(followedPostData)
     */
+
+    useEffect(() => {
+        let postsArray = []
+        viewFollowedPosts(user.id)
+        .then(res => {
+            console.log(res.data)
+            console.log(res.data.followed)
+            let followedArray = res.data.followed
+            followedArray.map(item => postsArray.push(...item.posts))
+            console.log(postsArray)
+            setData(postsArray)
+        })
+        .then(() => setLoading(false))
+    }, [])
+
+    const display = () => {
+        if(loading) {
+            return <ButtonSpinner />
+        } else {
+            return data.map((post, index) => (
+                <div key={post}>
+                    <div className="card-content white-text">
+                        <p>{post}</p>
+                    </div>
+                </div>
+            ))
+        }
+    }
 
     return (
         <>
         <div>
             <h1>Following Feed</h1>
+        </div>
+        <div>
+            Feed goes here
+            {display()}
         </div>
         </>
     )
