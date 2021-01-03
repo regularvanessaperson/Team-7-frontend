766938cab3fe379cd05e552d5b89f1660fb73c28
billymoroney <YOUR-EMAIL-ADDRESS>
2021-01-02 18:38:33 -0600

    make all tweet bodies render on the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,36 @@
 
+import React, {useState, useEffect} from 'react'
 import PostForm from "./PostForm"
 import Searchbar from "./Searchbar"
+import Post from "./Post"
+
+const axios = require('axios')
 
 
-const Home = () => {
+const Home = (props) => {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/posts/feed').
+        then(async (response) => {
+            await setPosts(response.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [])
+
+    const postsFeed = posts.map((post, index) => {
+        return <li>{post.body}</li>
+        // return <Post post={post} />
+    })
+
     return <div>
         <h1>Home Page</h1>
         <Searchbar />
         <PostForm />
+        {postsFeed}
+        
       
     </div>
     
