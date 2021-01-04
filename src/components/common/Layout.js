c5e008f56c1c107edee89cf1a403dac635244d06
nichro02 <nichro02@gmail.com>
2021-01-04 15:26:34 -0800

    Styled navbar to highlight active page in tab

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -37,9 +37,9 @@ const Layout = (props) => {
     <div>
       <nav className="navbar navbar-expand sticky-top navbar-dark bg-primary">
         <Link to="/" className="navbar-brand">
-          Cool Site
+          Chirper
         </Link>
-        <div className="navbar-nav mr-auto">
+        <div className="navbar-nav nav-tabs mr-auto">
           <li className="nav-item">
             <Link to={"/home"} className="nav-link">
               Home
@@ -47,7 +47,7 @@ const Layout = (props) => {
           </li>
           {showAdminBoard && (
             <li className="nav-item">
-              <Link to={"/admin"} className="nav-link">
+              <Link to={"/admin"} className="nav-link active" aria-current="page">
                 Admin Board
               </Link>
             </li>
