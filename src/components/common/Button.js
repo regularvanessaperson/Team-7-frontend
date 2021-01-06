432acb5cbaaa7e1f215d17e719ddefc7cde4ced7
regularvanessaperson <69284928+regularvanessaperson@users.noreply.github.com>
2021-01-06 01:53:25 -0600

    used icons for delete and edit (#72)

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,24 +1,19 @@
 import React from 'react'
 
 const Button = (props) => {
-//     const passingProps = props.label 
-//     // console.log("what is this", props.label)
-//     // Favorite- Retweet - Delete- Edit- Retweet
-// const label = (props) => {
-//     if (passingProps === edit) {
-//         // console.log("what is this", props.label.edit)
-//         return <i class="fas fa-edit"></i> 
-//     }else {
-//         return(
-//             {passingProps}
-//         ) 
-//     }
-// }
+    let passingProps = props.label 
+    
+    if(passingProps === 'Edit'){
+        passingProps = <i class="fas fa-edit"></i> 
+    }else if(passingProps === 'Delete'){
+        passingProps = <i class="fas fa-trash"></i>
+    }
+
     
     return (
         <button
         className = {props.className}
-        onClick={props.handleClick}>{props.label}</button>
+        onClick={props.handleClick}>{passingProps}</button>
     )
 }
 
